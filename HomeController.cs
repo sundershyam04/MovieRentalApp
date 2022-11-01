725fe29cc335186dd2bd4c6d82d0c5a0cddc4c22
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-11-01 16:16:32 +0530

    movierent webapi

---
+++ HomeController.cs
@@ -0,0 +1,20 @@
+﻿using MovieRentDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRent.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var c = new movierent();
+            ViewBag.Title = "Home Page";
+
+            return View(c.Movies);
+        }
+    }
+}
---
+++ MovieController.cs
@@ -0,0 +1,59 @@
+﻿using MovieRentDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieRent.Controllers
+{
+    public class MovieController : ApiController
+    {
+        movierent context;
+
+        public MovieController()
+        {
+           context = new movierent();
+        }
+        // GET api/<controller>
+        public List<Movie> Get()
+        {
+
+            return context.Movies.ToList();
+
+        }
+
+
+        // GET api/<controller>/5
+        public Movie Get(int id)
+        {
+            return context.Movies.ToList().Find(m => m.MovieId == id);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] Movie m)
+        {
+            context.Movies.Add(m);
+
+            context.SaveChanges();
+
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            Movie del =context.Movies.ToList().Find(m => m.MovieId == id);
+
+            context.Movies.Remove(del);
+
+            context.SaveChanges();
+
+        }
+    }
+}
